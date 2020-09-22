d66fecd007fb7e5fed7414bf6180a475f8163328
Mia <mia@Mias-MacBook-Pro.local>
2020-09-21 19:48:13 -0600

    user relationships defined

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -100,6 +100,11 @@ const BootcampSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true
+  }
 }, {
   toJSON: {
     virtuals: true
@@ -139,7 +144,9 @@ BootcampSchema.pre('save', async function (next) {
 //Cascade delete courses when a bootcamp is deleted
 BootcampSchema.pre('remove', async function (next) {
   console.log(`Courses being removed from bootcamp ${this._id}`);
-  await this.model('Course').deleteMany({ bootcamp: this._id });
+  await this.model('Course').deleteMany({
+    bootcamp: this._id
+  });
   next();
 });
 
