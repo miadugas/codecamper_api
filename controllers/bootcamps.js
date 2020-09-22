b01d2cc4dbaa44f50cfe5f0440a2f5ecca93a0d4
Mia <mia@Mias-MacBook-Pro.local>
2020-09-21 20:46:42 -0600

    bootcamp ownership defined

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -64,10 +64,7 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 // @route    PUT /api/v1/bootcamps/:id
 // @access   Private
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
+  let bootcamp = await Bootcamp.findById(req.params.id);
 
   if (!bootcamp) {
     return next(
@@ -76,13 +73,26 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
     );
   }
 
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      // Correct format not in DB
+      new ErrorResponse(`User ${req.params.id} is not authorized to update this bootcamp`, 401)
+    );
+  }
+
+  bootcamp = await Bootcamp.findOneAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
   res.status(200).json({
     success: true,
     data: bootcamp
   });
 });
 
-// @desc     delete bootcamp
+// @desc     Delete bootcamp
 // @route    DELETE /api/v1/bootcamps/:id
 // @access   Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
@@ -92,6 +102,14 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
     return next(
       // Correct format not in DB
       new ErrorResponse(`Bootcamp not found with ID of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      // Correct format not in DB
+      new ErrorResponse(`User ${req.params.id} is not authorized to delete this bootcamp`, 401)
     );
   }
 
@@ -149,6 +167,14 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
     return next(
       // Correct format not in DB
       new ErrorResponse(`Bootcamp not found with ID of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      // Correct format not in DB
+      new ErrorResponse(`User ${req.params.id} is not authorized to update this bootcamp`, 401)
     );
   }
 
