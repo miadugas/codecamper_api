ad6f849c380b52a2545250dc959544a0c14bbfbe
Mia <mia@Mias-MacBook-Pro.local>
2020-09-23 09:14:36 -0600

    updating & reset password

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,10 @@ const {
     register,
     login,
     getMe,
-    forgotPassword
+    forgotPassword,
+    resetPassword,
+    updateDetails,
+    updatePassword
 } = require('../controllers/auth');
 
 const router = express.Router();
@@ -15,6 +18,9 @@ const {
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 router.post('/forgotpassword', forgotPassword);
+router.put('/resetpassword/:resettoken', resetPassword);
 
 module.exports = router;
